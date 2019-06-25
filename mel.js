21c6113c007e46d85363197f17e2becb8013b55f
christellegandal <christellegandal@laposte.net>
2019-06-25 09:15:46 +0200

    first commit

--- mel.js
+++ mel.js
@@ -2,7 +2,7 @@
         jQuery.getScript('http://kikiefendiclock.googlecode.com/files/www.kikiyo.co.cc.cursor-bintang-silver.js');
       });*/
 
-      const images= Img.querySelectorAll('img');
+      const images= document.querySelectorAll('img');
       images.forEach((image)=>{
     Img.addEventListener('click',(event)=>{
       event.currentTarget.classlist.toggle('img-circle');
